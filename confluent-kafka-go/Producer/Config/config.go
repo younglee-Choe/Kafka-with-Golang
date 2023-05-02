e2bc2dc54c8d68eced9111630a95c4be88628a04
younglee-Choe <leele7111@naver.com>
2023-05-02 18:59:16 +0900

    delete unnecessary files

--- confluent-kafka-go/Producer/Config/config.go
+++ confluent-kafka-go/Producer/Config/config.go
@@ -1,15 +1,20 @@
 package Config
 
 import (
-	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func Kafka() *kafka.Producer {
 	p, err := kafka.NewProducer(&kafka.ConfigMap {
-		"bootstrap.servers": "203.247.240.235",
+		"bootstrap.servers": "localhost:9092",		// producer can find the Kakfa cluster
+		// "client.id": socket.gethostname(),		// easily correlate requests on the broker with the client instance
+    	"acks": "all",								// acks=all; guarantees fault tolerance
 	})
 	if err != nil {
-		// When a connection error occurs, a panic occurs and the system is shut down
+		// When a connection error occurs, 
+		// a panic occurs and the system is shut down
+		fmt.Printf("Failed to create producer: %s", err)
 		panic(err)
 	}
 
