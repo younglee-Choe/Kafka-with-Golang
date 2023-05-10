f9986b995558548867127fa1ddf730a56ce46294
younglee-Choe <leele7111@naver.com>
2023-05-10 20:34:39 +0900

    create messages using JSON file and send them to Kafka

---
+++ src/producer/producer.go
@@ -0,0 +1,60 @@
+package producer
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"main/producer/config"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func Producer() {
+    fmt.Println("🫧 Kafka Producer")
+
+	p := config.Kafka()
+	defer p.Close()
+
+	// Go-routine to handle message delivery reports and
+	// possibly other event types (errors, stats, etc)
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("❗️Failed to deliver message: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("🌿Produced event to topic %s: key = %-10s value = %s\n",
+					*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	// generate topic to send events
+	topic := "purchases"
+
+	// using JSON file
+	jsonFile, err := os.Open("./input-data/customers.json")
+	if err != nil {
+		fmt.Println("❗️Failed to open file;", err)
+	} else {
+		fmt.Println("Successfully Opened JSON file!")
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	if byteValue != nil {
+		p.Produce(&kafka.Message{
+            TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			// Key:			[]byte(key)
+            Value:          byteValue,
+        }, nil)
+	} else {
+		fmt.Printf("❗️There is no data to send to Kafka")
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15*1000)
+}
