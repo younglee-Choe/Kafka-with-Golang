d6655a4d689faa6211db3be54f3f73e09b5188fd
younglee-Choe <leele7111@naver.com>
2023-05-18 14:54:14 +0900

    go structures

---
+++ src/structures/structures.go
@@ -0,0 +1,34 @@
+package structures
+
+// type Tasks struct {
+// 	Task []Task `json:"tasks"`
+// }
+
+type Task struct {
+    CreatedAt string	`json:"createdAt"`
+    Name string			`json:"name"`
+    Avatar string		`json:"avatar"`
+	Agee int32 			`json:"agee"`
+	Completed bool 		`json:"completed"`
+	Id string 			`json:"id"`
+}
+
+type Street struct {
+	Name string `json:"name"`
+	Address string `json:"address"`
+}
+
+type Customers struct {
+	Customer []Customer `json:"customers"`
+}
+
+type Customer struct {
+    Name string	`json:"name"`
+    Age int32	`json:"age"`
+    Items Items	`json:"items"`
+}
+
+type Items struct {
+	Snack string `json:"snack"`
+	Fruit string `json:"fruit"`
+}
