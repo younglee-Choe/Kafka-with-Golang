5ecf4842641cb05acd2670510a4196b0faa60121
younglee-Choe <leele7111@naver.com>
2023-05-18 14:47:42 +0900

    subscribe to topic on which processed data exists

--- src/consumer/consumer.go
+++ src/consumer/consumer.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-    "main/consumer/config"
+	"main/consumer/config"
 )
 
 func main() {
@@ -11,17 +11,18 @@ func main() {
 	c := config.Kafka()
 
 	// subscribe topic or multiple tipics
-	c.SubscribeTopics([]string{"purchases", "tasks"}, nil)
+	c.SubscribeTopics([]string{"boss_topic"}, nil)
 	defer c.Close()
 
+	// var customers structures.Customers
 	for {
 		// ReadMessage polls the consumer for a message.
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
 			// msg.TopicPartition provides partition-specific information (such as topic, partition and offset).
-			fmt.Printf("🌿Message on %s: key=%s, vlaue=%s\n", msg.TopicPartition, msg.Key, msg.Value)
+			fmt.Printf("🍺 Received message %s: key=%s, vlaue=%s\n", msg.TopicPartition, msg.Key, msg.Value)
 		} else {
 			fmt.Printf("❗️Consumer error: %v (%v)\n", err, msg)
-		}
+	}
 	}
 }
