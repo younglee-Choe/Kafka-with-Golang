e256ae44f1af5fa5a4fe3cd554dc91b9c044301f
younglee-Choe <leele7111@naver.com>
2023-07-10 11:12:12 +0900

    create multiple processors for fountain decoding

--- src/goka/processing/processing.go
+++ src/goka/processing/processing.go
@@ -4,34 +4,37 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"time"
 	"strconv"
+	"time"
+
+	"main/src/fountain"
 
 	"github.com/lovoo/goka"
 	"github.com/lovoo/goka/codec"
-	"github.com/lovoo/goka/storage"
 )
 
 var (
-	brokers             = []string{"kafkahost:9092"}
-	topic   goka.Stream = "topic"
-	topicRekeyed   goka.Stream = "topic-rekeyed"
-	group   goka.Group  = "goka-group"
-	st    storage.Storage
+	brokers                  = []string{"203.247.240.235:9092", "203.247.240.235:9093", "203.247.240.235:9094"}
+	topic        goka.Stream = "leele-topic"
+	topicRekeyed goka.Stream = "leele-topic-rekeyed"
+	groupEncoded goka.Group  = "leele-group-encoded"
+	groupDecoded goka.Group  = "leele-group-decoded"
 
-	tmc *goka.TopicManagerConfig
-	producerSize int = 8
-	defaultPartitionChannelSize = producerSize
+	tmc                         *goka.TopicManagerConfig
+	producerSize                int = 8
+	defaultPartitionChannelSize     = producerSize
 )
 
-type user struct {
-	Counter int
-	Data string
+type block struct {
+	Counter     int
+	EncodedData string
+	DecodedData []byte
 }
 
 // This codec allows marshalling (encode) and unmarshalling (decode) the user to and from the group table
-type userCodec struct{}
+type blockCodec struct{}
 
 func init() {
 	tmc = goka.NewTopicManagerConfig()
@@ -40,17 +43,17 @@ func init() {
 }
 
 // Encodes a user into []byte
-func (jc *userCodec) Encode(value interface{}) ([]byte, error) {
-	if _, isUser := value.(*user); !isUser {
+func (jc *blockCodec) Encode(value interface{}) ([]byte, error) {
+	if _, isUser := value.(*block); !isUser {
 		return nil, fmt.Errorf("Codec requires value *user, got %T", value)
 	}
 	return json.Marshal(value)
 }
 
 // Decodes a user from []byte to it's go representation
-func (jc *userCodec) Decode(data []byte) (interface{}, error) {
+func (jc *blockCodec) Decode(data []byte) (interface{}, error) {
 	var (
-		c   user
+		c   block
 		err error
 	)
 	err = json.Unmarshal(data, &c)
@@ -73,62 +76,107 @@ func runEmitter() {
 
 	var i int
 	for range t.C {
-		key := fmt.Sprintf("user-%d", i%8)	
-		value := fmt.Sprintf("user%d", i%8)		// Value: fountain encoded data
+		key := fmt.Sprintf("user-%d", i%10)
+		value := fmt.Sprintf("user%d", i%10) // Value: fountain encoded data
+		fmt.Printf("key: %s, value: %s\n", key, value)
 		emitter.EmitSync(key, value)
 		i++
 	}
 }
 
+// Rekeyed: blockID -> index(offset)
 func process(ctx goka.Context, msg interface{}) {
 	key := ctx.Offset()
 	ctx.Loopback(strconv.Itoa(int(key)), msg)
 }
 
+// Update the state
 func loopProcess(ctx goka.Context, msg interface{}) {
-	var u *user
+	var b *block
 	if val := ctx.Value(); val != nil {
-		u = val.(*user)
+		b = val.(*block)
 	} else {
-		u = new(user)
+		b = new(block)
 	}
 
-	if u.Counter < producerSize {
-		u.Counter++
+	if b.Counter < producerSize {
+		b.Counter++
 
-		// previous state + current input
-		u.Data += (msg.(string) + ", ")
+		// Stores the encoded data(source data) sent by the Producer by offset (size: the number of Producer)
+		b.EncodedData += (msg.(string) + ", ")
+
+		// source data가 다 모였으면, 데이터들 조합해서 디코딩 시도
+		// combinationAlgorithm()
 	} else {
 		fmt.Println("❕Reset")
-		u.Counter = 0
+		b.Counter = 0
 	}
 
-	ctx.SetValue(u)
-
-	// Try decoding
-	fountainDecoding(ctx)
-
+	ctx.SetValue(b)
 	fmt.Printf("Partition: %d, Offset: %d, Key: %s, Value: %s, msg: %v\n", ctx.Partition(), ctx.Offset(), ctx.Key(), ctx.Value(), msg)
 }
 
-func fountainDecoding(ctx goka.Context) {
-	// write decode part...
-	test := "20"
-
-	// write if-else; emit only decoded data
-	if ctx.Key() == test {
-		fmt.Println("‼️ Decoding Success")
-		ctx.Emit(topicRekeyed, ctx.Key(), "decoded data")
+// table에 디코딩된 데이터들 저장(stateful-based)
+func secondProcess(ctx goka.Context, msg interface{}) {
+	var b *block
+	if val := ctx.Value(); val != nil {
+		b = val.(*block)
+	} else {
+		b = new(block)
 	}
+
+	decodedData := fountainDecoding(ctx, msg)
+	b.DecodedData = append(b.DecodedData, decodedData...)
+
+	ctx.SetValue(b)
+
+	// 성공적으로 디코딩된 결과만 새로운 topic에 저장
+	ctx.Emit(topicRekeyed, ctx.Key(), "Successfully decoded data")
+}
+
+func combinationAlgorithm() {
+	// Producer가 보낸 데이터들을 조합(nCn-1) 알고리즘으로 묶어서 디코딩 시도
+
+	// 디코딩 결과를 Kafka에 저장(stateful-based)
+	// decodedData := fountainDecoding(ctx, msg)
+	// u.DecodedData = append(u.DecodedData, decodedData...)
+}
+
+func fountainDecoding(ctx goka.Context, msg interface{}) []byte {
+	filePath := "../../dummy_data/small_dummy.json"
+	filePath2 := "../../dummy_data/small_malicious_dummy.json"
+
+	message, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	malicious_message, err := ioutil.ReadFile(filePath2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	serializedEncodedBlocks := make([][]byte, 3)
+
+	serializedEncodedBlocks[0] = fountain.EncodeM(message, 8923483)
+	serializedEncodedBlocks[1] = fountain.EncodeM(message, 8923486)
+	serializedEncodedBlocks[2] = fountain.EncodeM(malicious_message, 8923487)
+
+	out, err := fountain.Decode(serializedEncodedBlocks, len(message))
+
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	return out
 }
 
 func runProcessor(initialized chan struct{}) {
-	g := goka.DefineGroup(group,
+	g := goka.DefineGroup(groupEncoded,
 		goka.Input(topic, new(codec.String), process),
 		goka.Loop(new(codec.String), loopProcess),
-		// goka.Output(topicRekeyed, new(userCodec)),
-		goka.Output(topicRekeyed, new(codec.String)),
-		goka.Persist(new(userCodec)),
+		// goka.Output(topicRekeyed, new(codec.String)),
+		goka.Persist(new(blockCodec)),
 	)
 	p, err := goka.NewProcessor(brokers,
 		g,
@@ -146,12 +194,34 @@ func runProcessor(initialized chan struct{}) {
 	}
 }
 
+func runProcessor2(initialized chan struct{}) {
+	g := goka.DefineGroup(groupDecoded,
+		goka.Input(topic, new(codec.String), secondProcess),
+		goka.Output(topicRekeyed, new(codec.String)),
+		goka.Persist(new(blockCodec)),
+	)
+	p, err := goka.NewProcessor(brokers,
+		g,
+		goka.WithTopicManagerBuilder(goka.TopicManagerBuilderWithTopicManagerConfig(tmc)),
+		goka.WithConsumerGroupBuilder(goka.DefaultConsumerGroupBuilder),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	close(initialized)
+
+	if err = p.Run(context.Background()); err != nil {
+		log.Printf("Error running processor2: %v", err)
+	}
+}
+
 func runView(initialized chan struct{}) {
 	<-initialized
 
 	view, err := goka.NewView(brokers,
-		goka.GroupTable(group),
-		new(userCodec),
+		goka.GroupTable(groupDecoded),
+		new(blockCodec),
 	)
 	if err != nil {
 		panic(err)
@@ -177,5 +247,6 @@ func main() {
 
 	go runEmitter()
 	go runProcessor(initialized)
+	go runProcessor2(initialized)
 	runView(initialized)
 }
