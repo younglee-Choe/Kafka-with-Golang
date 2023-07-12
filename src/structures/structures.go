72637f661ee96860e3bb5783a60d8e25a79641f3
younglee-Choe <leele7111@naver.com>
2023-07-12 23:22:46 +0900

    Go structs for detecting insecure decoding

--- src/structures/structures.go
+++ src/structures/structures.go
@@ -1,44 +1,7 @@
 package structures
 
-// type Tasks struct {
-// 	Task []Task `json:"tasks"`
-// }
-
 type Block struct {
-	Id string `json:"id"`
-	Value string `json:"value"`
-}
-
-type Person struct {
-	Name string `json:"name"`
+	Name    string `json:"name"`
 	Account string `json:"account"`
-}
-
-type Task struct {
-    CreatedAt string	`json:"createdAt"`
-    Name string			`json:"name"`
-    Avatar string		`json:"avatar"`
-	Agee int32 			`json:"agee"`
-	Completed bool 		`json:"completed"`
-	Id string 			`json:"id"`
-}
-
-type Street struct {
-	Name string `json:"name"`
-	Address string `json:"address"`
-}
-
-type Customers struct {
-	Customer []Customer `json:"customers"`
-}
-
-type Customer struct {
-    Name string	`json:"name"`
-    Age int32	`json:"age"`
-    Items Items	`json:"items"`
-}
-
-type Items struct {
-	Snack string `json:"snack"`
-	Fruit string `json:"fruit"`
+	Id      string `json:"id"`
 }
